0eef4f8bdd2510b45553399fabf9ff6a9a5ae3d7
Julio Sarachaga <julillosamaral@gmail.com>
2014-05-01 23:40:07 -0300

    Initial commit

---
+++ module/RBac/config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RBac;
+
+return array(
+   'doctrine' => array(
+       'driver' => array(
+           __NAMESPACE__ . '_driver' => array(
+              'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+              'cache' => 'array',
+              'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+           ),
+           'orm_default' => array(
+               'drivers' => array(
+                   __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+           )
+        )
+     )
+  ),
+
+  'zfcuser' => array(
+     'user_entity_class' => 'RBac\Entity\User',
+     'enable_default_entities' => false,
+  )
+);
